ee6d82ed8a3524b5f95a665a8c7a905c1eab49c2
Tom Nicklin <t-nicklin@hotmail.co.uk>
2016-10-04 16:47:58 +0100

    bebop.py is working as intended, now just appearence and extra information. bebop.py has been compiled to .pyc as well as waypointer.py due to subtle changes. New directory /bebop/ has been added for future as it has a pud to csv function that is useful for others in future.

--- bebop.py
+++ bebop.py
@@ -1,18 +1,27 @@
 import csv
 import simplemap
+import webbrowser
 
-csvfile = 'test4.csv'
+csvfile = 'test3.pud.csv'
 
-def get_product_coordinates(file):
+def get_coordinates(file):
 	with open(file, 'rb') as csvfile:
 
 		# printing specifics
 		readmorelikecashmore = csv.DictReader(csvfile)
 
 
-		coordinates = [ [ 'altitude: ' + row['altitude'] + 'cm, flying state: ' + row['flying_state'], float(row['product_gps_longitude']), float(row['product_gps_latitude']) ] for row in readmorelikecashmore]
+		coordinates = [ [ 'altitude: ' + row['altitude'] + 'cm, flying state: ' + row['flying_state'], float(row['product_gps_latitude']), float(row['product_gps_longitude']) ] for row in readmorelikecashmore]
 		
-		return coordinates
+		# When we get the gps coordinates, some of them are 500. That doesn't exist on maps, as a result gmaps just doesn't function.
+		# Here we santise (ignore) those results.
+		sanitised = []
+		for x in range(0, len(coordinates)):
+			# If legitimate gps locations then the following if should be true.
+			if((coordinates[x][1] or coordinates[x][2]) < 179):
+				sanitised.append(coordinates[x])
+		# Finally we return the result.
+		return sanitised
 
 # Here we change the formatting of the waypoints so gmaps api can use them to plot lines
 def make_points(coords):
@@ -41,15 +50,14 @@ def make_points(coords):
 map_title = csvfile
 
 # This gets the coordinates from the above function 
-#gps_markers = get_product_coordinates(csvfile) 
+gps_markers = get_coordinates(csvfile)
 
-# need to make a fake coordinates right now because 
-# the drone logs doesn't have real ones.
-# TODO: uncomment the real line below this test one.
-gps_markers = [ ['0', 23, 7], ['1', 24, 6], ['2', 25, 8] ]
-#gps_markers = get_product_coordinates(csvfile)
-
+# We need a clone of gps_markers because python assigns by reference. 
+# In short it means if you change the copy you'll ALSO change the original. Not good. 
+# On top of that gps_markers is a 2D list so it requires a bit more thought.
 new_gps = [sublist[:] for sublist in gps_markers]
+
+# Use said copy to do something different without effecting the original.
 plots = make_points(new_gps)
 
 
@@ -61,7 +69,7 @@ example_map.write(csvfile + '.html')
 
 # Finally we finish the script by opening the html
 # file with whatever is the defult browser
-#webbrowser.open_new('sequence' + str(sequence) + '.html')
+webbrowser.open_new(csvfile + '.html')
 
 # Give some indication that the process has finished and now we just open the html file.
 print '\nWriting ' + csvfile + '.hmtl...'
